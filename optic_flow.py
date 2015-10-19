7f91b46419c619a856518a71fe2b8e367170b581
Maoz Melamed <maoz@minute.ly>
2015-10-19 23:41:53 +0300

    Python Conventions

--- optic_flow.py
+++ optic_flow.py
@@ -6,83 +6,80 @@ import cv2
 
 class OpticFlow:
 
-	def __init__(self, file_name):
-		self.file_name = file_name
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-	def get_optic_flow_diff(self):
-		cap = cv2.VideoCapture(self.file_name)
+    def get_optic_flow_diff(self):
+        cap = cv2.VideoCapture(self.file_name)
 
-		# params for ShiTomasi corner detection
-		feature_params = dict( maxCorners = 100,
-													qualityLevel = 0.3,
-													minDistance = 7,
-													blockSize = 7 )
+        # params for ShiTomasi corner detection
+        feature_params = dict(maxCorners=100,
+                              qualityLevel=0.3,
+                              minDistance=7,
+                              blockSize=7)
 
-		# Parameters for lucas kanade optical flow
-		lk_params = dict( winSize  = (15,15),
-										 maxLevel = 2,
-										 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        # Parameters for lucas kanade optical flow
+        lk_params = dict(winSize=(15, 15),
+                         maxLevel=2,
+                         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-		# Take first frame and find corners in it
-		ret, old_frame = cap.read()
-		old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-		p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+        # Take first frame and find corners in it
+        ret, old_frame = cap.read()
+        old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+        p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
 
-		dist_arr = []
+        dist_arr = []
 
-		while(1):
-			ret,frame = cap.read()
-			
-			if frame == None:
-				break
+        while(1):
+            ret, frame = cap.read()
 
-			frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if frame is None:
+                break
 
-			# calculate optical flow
-			# print "frame_gray %s" % frame_gray
-			# print "old_gray %s" % old_gray
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-			if not np.any(old_gray) or not np.any(frame_gray):
-				old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-				dist_arr.append(None)
-				print old_gray
-				print frame_gray
-				continue
+            # calculate optical flow
+            # print "frame_gray %s" % frame_gray
+            # print "old_gray %s" % old_gray
 
-			p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-			
-			# Select good points
-			if p1 == None:
-				dist_arr.append(None)
-				old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-				continue
+            if not np.any(old_gray) or not np.any(frame_gray):
+                old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+                dist_arr.append(None)
+                print old_gray
+                print frame_gray
+                continue
 
-			good_new = p1[st==1]
-			good_old = p0[st==1]
+            p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-			dist_arr.append(dist(good_new,good_old)) 
+            # Select good points
+            if p1 is None:
+                dist_arr.append(None)
+                old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+                continue
 
-			# Now update the previous frame and previous points
-			old_gray = frame_gray.copy()
-			p0 = good_new.reshape(-1,1,2)
+            good_new = p1[st == 1]
+            good_old = p0[st == 1]
 
-		cap.release()
-		return dist_arr
+            dist_arr.append(dist(good_new, good_old))
+
+            # Now update the previous frame and previous points
+            old_gray = frame_gray.copy()
+            p0 = good_new.reshape(-1, 1, 2)
+
+        cap.release()
+        return dist_arr
 
 if __name__ == '__main__':
-	INPUT_FILE = 'GoPro.mp4'
-	OUTPUT_FILE = 'GoPro_2fps.mp4'
-	FPS = 2
+    INPUT_FILE = 'videos/RoofJump.mp4'
+    OUTPUT_FILE = 'videos/RoofJump_2fps.mp4'
+    FPS = 2
 
-	vp = VideoProccessor(INPUT_FILE)
-	file_name = vp.encode(output_file = OUTPUT_FILE ,fps = FPS)
+    vp = VideoProccessor(INPUT_FILE)
+    file_name = vp.encode(output_file=OUTPUT_FILE, fps=FPS)
 
-	of = OpticFlow(file_name)
-	diff = of.get_optic_flow_diff()
-	print diff
-	print 'done'
-
-
-
+    of = OpticFlow(file_name)
+    diff = of.get_optic_flow_diff()
+    print diff
+    print 'done'
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def dist(mat_a, mat_b):
-	distance = (mat_a - mat_b)**2
-	distance = np.sum(distance, axis=1)
-	distance = np.sqrt(distance)
-	return np.mean(distance)
 
+def dist(mat_a, mat_b):
+    distance = (mat_a - mat_b)**2
+    distance = np.sum(distance, axis=1)
+    distance = np.sqrt(distance)
+    return np.mean(distance)
